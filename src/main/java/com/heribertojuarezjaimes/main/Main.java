6bbf5dce32620d7be9b4b23a75066327105d7fc2
Heriberto <heribertojuarezj@gmail.com>
2025-01-07 12:23:21 -0600

    Handle thread exceptions

--- src/main/java/com/heribertojuarezjaimes/main/Main.java
+++ src/main/java/com/heribertojuarezjaimes/main/Main.java
@@ -6,16 +6,26 @@ public class Main {
         Thread thread = new Thread(()->{
             // Code that will run in a new thread.
             System.out.println("Hello, Im inside the thread that was started: " + Thread.currentThread().getName());
+            throw new RuntimeException("Intentional Exception");
         });
 
         thread.setName("My Thread #1 - IntelliJ");
         thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
-        thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
 
         Thread.sleep(10000);
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread " + t.getName() + " the error is: " + e.getMessage());
+            }
+        });
+
+        thread.start();
+
 
     }
 
